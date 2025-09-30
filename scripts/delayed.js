580d132bf8f7e1346bf8b11e27cbf0311462b00d
Charity Helms <chelms@adobe.com>
2025-09-30 07:42:08 -0400

    comment out customer-specific code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-cycle
-import { loadScript } from './aem.js';
+// import { loadScript } from './aem.js';
 
-// we should put these in placeholders eventually
+// commenting out customer scripts here but leaving as an example
+/*
 const launchDev = 'https://assets.adobedtm.com/acb96670c057/48663f28f53f/launch-e24016a2c101-development.min.js';
 const launchProd = 'https://assets.adobedtm.com/acb96670c057/48663f28f53f/launch-450e00021d4f.min.js';
 const oneTrustId = 'fab99249-4680-4892-8016-c1821d0ca04a';
@@ -26,3 +27,4 @@ if (oneTrustId) {
 const script = document.createElement('script');
 script.src = `https://cdn.cookielaw.org/scripttemplates/otSDKStub.js?domain=${oneTrustId}`;
 document.head.appendChild(script);
+*/
